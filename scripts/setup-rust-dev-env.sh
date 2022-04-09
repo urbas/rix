61612c58d00e1968a664e5b58986954cab4a2eab
Matej Urbas <matej.urbas@gmail.com>
2022-04-09 10:58:15 +0100

    update: rust 1.60.0, devEnv

--- scripts/setup-rust-dev-env.sh
+++ scripts/setup-rust-dev-env.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 RUST_VERSION=$(<.rust-version)
 rustup toolchain install $RUST_VERSION
+
+# It would be nicer if vscode could load the version of rust from the `.rust-version` file.
+sed -i "s/\"RUSTUP_TOOLCHAIN\":.*,$/\"RUSTUP_TOOLCHAIN\": \"${RUST_VERSION}\",/" $SCRIPT_DIR/../.vscode/settings.json
+
 export PATH=$HOME/.rustup/toolchains/$(rustup toolchain list | grep --fixed-string $RUST_VERSION | head -n1)/bin:$PATH
