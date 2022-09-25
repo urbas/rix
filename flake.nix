bb3c149cc834192f22aec38c8e05cb693b11291a
Matej Urbas <matej.urbas@gmail.com>
2022-09-25 11:01:46 +0100

    dev env improved and documented

--- flake.nix
+++ flake.nix
@@ -3,27 +3,22 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: rec {
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    nixpkgsForAllSystems = forAllSystems (system:
-      import nixpkgs { inherit system; }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: f { pkgs = import nixpkgs { inherit system; }; });
 
-    packages = nixpkgsForAllSystems;
-
-    devShell = forAllSystems (system:
-      with nixpkgsForAllSystems.${system};
-
-      stdenv.mkDerivation {
-        name = "rix";
-        buildInputs = [
-          busybox-sandbox-shell
-          coreutils
-          nix
-          rustup
-        ];
-      }
-    );
-  };
+    in {
+      packages = forAllSystems ({pkgs}: pkgs);
+      devShells = forAllSystems ({pkgs}: with pkgs; {
+        default = stdenv.mkDerivation {
+          name = "rix";
+          buildInputs = [
+            busybox-sandbox-shell
+            coreutils
+            nix
+            rustup
+          ];
+        };
+      });
+    };
 }
