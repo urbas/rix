41e5e1977e11278ffa5c3498e67a7e14cb4cb651
Matej Urbas <matej.urbas@gmail.com>
2022-10-20 19:00:18 +0100

    BuildConfig now uses File instead of RawFd for stdout and stderr

--- src/building/mod.rs
+++ src/building/mod.rs
@@ -1,8 +1,7 @@
 use crate::derivations::Derivation;
 use crate::sandbox::{mount_paths, run_in_sandbox};
 use std::fs::File;
-use std::os::unix::io::FromRawFd;
-use std::os::unix::io::{AsRawFd, RawFd};
+use std::os::unix::io::{AsRawFd, FromRawFd};
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::{Command, Stdio};
@@ -10,8 +9,8 @@ use std::process::{Command, Stdio};
 pub struct BuildConfig<'a> {
     derivation: &'a Derivation,
     build_dir: &'a Path,
-    stdout_fd: Option<RawFd>,
-    stderr_fd: Option<RawFd>,
+    stdout: Option<&'a File>,
+    stderr: Option<&'a File>,
 }
 
 impl<'a> BuildConfig<'a> {
@@ -19,26 +18,25 @@ impl<'a> BuildConfig<'a> {
         BuildConfig {
             derivation: derivation,
             build_dir: build_dir,
-            stderr_fd: None,
-            stdout_fd: None,
+            stderr: None,
+            stdout: None,
         }
     }
 
-    pub fn stdout_to_file(&mut self, file: &'a File) -> &mut Self {
-        self.stdout_fd = Some(file.as_raw_fd());
-        self
+    pub fn stdout_to_file(&mut self, file: &'a File) {
+        self.stdout = Some(file);
     }
 
-    pub fn stderr_to_file(&mut self, file: &'a File) -> &mut Self {
-        self.stderr_fd = Some(file.as_raw_fd());
-        self
+    pub fn stderr_to_file(&mut self, file: &'a File) {
+        self.stderr = Some(file);
     }
 }
 
 pub fn build_derivation_sandboxed(config: &BuildConfig) -> Result<i32, String> {
     // this function assumes all derivation inputs are present and won't be
     // GC'd for the duration of this build
-
+    let stdout_fd = config.stdout.map(|file| file.as_raw_fd());
+    let stderr_fd = config.stderr.map(|file| file.as_raw_fd());
     // return value is the error code of the builder or 255 if anything went
     // wrong and we failed to execute the builder
     run_in_sandbox(
@@ -46,10 +44,10 @@ pub fn build_derivation_sandboxed(config: &BuildConfig) -> Result<i32, String> {
         |new_root| mount_paths(config.derivation.input_srcs.iter().map(Path::new), new_root),
         || {
             let mut cmd = build_derivation_command(&config.derivation, &Path::new("/"));
-            if let Some(stdout_fd) = config.stdout_fd {
+            if let Some(stdout_fd) = stdout_fd {
                 cmd.stdout(unsafe { Stdio::from_raw_fd(stdout_fd) });
             }
-            if let Some(stderr_fd) = config.stderr_fd {
+            if let Some(stderr_fd) = stderr_fd {
                 cmd.stderr(unsafe { Stdio::from_raw_fd(stderr_fd) });
             }
             let exec_error = cmd.exec();
