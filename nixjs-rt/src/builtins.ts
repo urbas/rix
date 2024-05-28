e99c70fb7a9ff73a25754cdad18c68fb8ac9a846
Arduano <leonid.shchurov@gmail.com>
2024-05-29 07:45:42 +1000

    "isXXX" builtins
    Builtins for testing whether a value is of a certain type, should be relatively simple

--- nixjs-rt/src/builtins.ts
+++ nixjs-rt/src/builtins.ts
@@ -5,20 +5,26 @@ import { typeMismatchError } from "./errors/typeError";
 import {
   Attrset,
   EvalCtx,
-  EvalException,
-  FALSE,
   Lambda,
-  NULL,
+  NixBool,
   NixFloat,
   NixInt,
   NixList,
+  NixNull,
   NixString,
   NixType,
   NixTypeClass,
   Path,
-  TRUE,
 } from "./lib";
-import { dirOf, isAbsolutePath, normalizePath } from "./utils";
+import {
+  FALSE,
+  NULL,
+  TRUE,
+  dirOf,
+  isAbsolutePath,
+  nixBoolFromJs,
+  normalizePath,
+} from "./utils";
 
 type BuiltinsRecord = Record<string, (param: NixType) => NixType>;
 
@@ -385,39 +391,39 @@ export function getBuiltins() {
     },
 
     isAttrs: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof Attrset);
     },
 
     isBool: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixBool);
     },
 
     isFloat: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixFloat);
     },
 
     isFunction: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof Lambda);
     },
 
     isInt: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixInt);
     },
 
     isList: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixList);
     },
 
     isNull: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixNull);
     },
 
     isPath: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof Path);
     },
 
     isString: (arg) => {
-      throw new Error("unimplemented");
+      return nixBoolFromJs(arg instanceof NixString);
     },
 
     length: (arg) => {
